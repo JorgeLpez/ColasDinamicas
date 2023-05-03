de4105bf972e7a17f6232465e7630d19c1b16ee1
Jorge López <123602935+JorgeLpez@users.noreply.github.com>
2023-05-02 22:36:14 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,62 +1,240 @@
 #include <iostream>
-#include "Cola.h"
-#include "Constancia.h"
 
-using namespace std;
+class Constancia{
+    private:
+        std::string NombreAlm,NombreCarr;
+        int MatAprob;
+        float Prom;
+    public:
+        Constancia(){};
+
+        friend std::ostream & operator<<(std::ostream &O, Constancia &x){
+            O<<"\nNombre Alumno: "<<x.NombreAlm<<std::endl;
+            O<<"Nombre de la Carrera: "<<x.NombreCarr<<std::endl;
+            O<<"Total de materias aprobadas: "<<x.MatAprob<<std::endl;
+            O<<"Promedio general: "<<x.Prom<<std::endl;
+            return O;
+        }
+
+        friend std::istream & operator>>(std::istream &O, Constancia &x){
+            std::cout<<"\nNombre Alumno: ";
+            O>>x.NombreAlm;
+            std::cout<<"Nombre de la Carrera: ";
+            O>>x.NombreCarr;
+            std::cout<<"Total de materias aprobadas: ";
+            O>>x.MatAprob;
+            std::cout<<"Promedio general: ";
+            O>>x.Prom;
+            return O;
+        }
+
+        bool operator==(Constancia& c)
+        {
+            if(c.NombreAlm==NombreAlm && c.NombreCarr==NombreCarr && c.MatAprob==MatAprob && c.Prom==Prom){
+            return true;
+            }
+            return false;
+        }
+
+        bool operator!=(Constancia& c)
+        {
+            if(c.NombreAlm!=NombreAlm && c.NombreCarr!=NombreCarr && c.MatAprob!=MatAprob && c.Prom!=Prom){
+            return true;
+            }
+            return false;
+
+        }
+
+};
+
+template<class T>
+class Cola;
+
+template<class T>
+class node{
+private:
+    T data;
+    node<T>* sig;
+public:
+    node():sig(nullptr){};
+    friend class Cola<T>;
+};
+
+template<class T>
+class Cola{
+private:
+    node<T>* lista;
+    node<T>* ultimo()const;
+    node<T>* primero()const;
+    node<T>* anterior(node<T>* pos)const;
+    void insertar(node<T>* pos, T elem);
+    bool eliminar(node<T>* pos);
+public:
+    int Buscar(T elem)const;
+    Cola():lista(nullptr){};
+    bool vacia()const;
+    void imprimir()const;
+    void Enqueue(T elem);
+    void Dequeue();
+    T Front();
+};
+
+template<class T>
+int Cola<T>::Buscar(T elem)const{
+    node<T>* aux=lista;
+    int cont=0;
+    while(aux!=nullptr && aux->data!=elem){
+        aux = aux->sig;
+        cont ++;
+    }
+    return cont;
+}
+
+template<class T>
+T Cola<T>::Front(){
+    if(!vacia()){
+       return primero()->data;
+    }
+}
+
+template<class T>
+void Cola<T>::Dequeue(){
+    eliminar(primero());
+}
+
+template<class T>
+void Cola<T>::Enqueue(T elem){
+    insertar(ultimo(),elem);
+}
+
+template<class T>
+void Cola<T>::imprimir()const{
+    node<T>* aux=lista;
+    while(aux!=nullptr){
+        std::cout<<aux->data<<"-> ";
+        aux=aux->sig;
+    }
+}
+
+template<class T>
+bool Cola<T>::eliminar(node<T>* pos){
+    if(vacia() || pos==nullptr){
+        return false;
+    }
+    if(pos==lista){
+        lista=lista->sig;
+    }
+    else{
+        anterior(pos)->sig=pos->sig;
+    }
+    delete pos;
+    return true;
+}
+
+template<class T>
+void Cola<T>::insertar(node<T>* pos, T elem){
+    node<T>* aux= new node<T>;
+    aux->data=elem;
+    if(pos==nullptr){
+        aux->sig=lista;
+        lista=aux;
+    }
+    else{
+        aux->sig=pos->sig;
+        pos->sig=aux;
+    }
+}
+
+template<class T>
+node<T>* Cola<T>::anterior(node<T>* pos)const{
+    if(vacia() || pos==nullptr){
+        return nullptr;
+    }
+    node<T>* aux=lista;
+    while(aux!=nullptr && aux->sig!=pos){
+        aux=aux->sig;
+    }
+    return aux;
+}
+
+template<class T>
+node<T>* Cola<T>::primero()const{
+    if(vacia()){
+        return nullptr;
+    }
+    return lista;
+}
+
+
+template<class T>
+node<T>* Cola<T>::ultimo()const{
+    if(vacia()){
+        return nullptr;
+    }
+    node<T>* aux=lista;
+    while(aux->sig!=nullptr){
+        aux=aux->sig;
+    }
+    return aux;
+}
+
+template<class T>
+bool Cola<T>::vacia()const{
+    if(lista==nullptr)
+        return true;
+    return false;
+}
 
 int main()
 {
     int opc,cont;
-    Cola<Constancia> micola;
-    Constancia X;
+    Cola<Constancia> Micola;
+    Constancia x;
     do{
         system("cls");
-        cout<<"\n\tCONSTANCIAS DE ESTUDIO"<<endl<<endl;
-        cout<<"1. Generar solicitud para constancia."<<endl;
-        cout<<"2. Recibir constancia."<<endl;
-        cout<<"3. Buscar solicitud."<<endl;
-        cout<<"4. Salir."<<endl<<endl;
-        cout<<"Ingrese una opcion: ";
-        cin>>opc;
+        std::cout<<"\n\tCONSTANCIAS DE ESTUDIO"<<std::endl<<std::endl;
+        std::cout<<"1. Generar solicitud para constancia."<<std::endl;
+        std::cout<<"2. Recibir constancia."<<std::endl;
+        std::cout<<"3. Buscar solicitud."<<std::endl;
+        std::cout<<"4. Salir."<<std::endl<<std::endl;
+        std::cout<<"Ingrese una opcion: ";
+        std::cin>>opc;
 
         switch(opc){
 
             case 1: system("cls");
                     cont++;
-                    cout<<"\n\tUsted tiene el turno #"<<cont<<" Ingrese sus datos: "<<endl;
-                    cin>>X;
-                    if(micola.Enqueue(X)){
-                        cout<<"\n"<<endl;
-                    }else{
-                        cout<<"\nError!, La Cola esta llena."<<endl;
-                    }
+                    std::cout<<"\n\tUsted tiene el turno #"<<cont<<" Ingrese sus datos: "<<std::endl;
+                    std::cin>>x;
+                    Micola.Enqueue(Constancia(x));
+                    std::cout<<"Constancia agregada exitosamente!"<<std::endl<<std::endl;
                     system("pause");
                     break;
 
             case 2: system("cls");
-                    if(micola.Vacia()){
-                        cout<<"La cola esta vacia."<<endl;
+                    if(Micola.vacia()){
+                        std::cout<<"La cola esta vacia."<<std::endl;
                     }else{
-                        cout<<"\n\tElaborando constancia: "<<endl;
-                        X=micola.Front();
-                        cout<<X<<endl;
+                        std::cout<<"\n\tElaborando constancia: "<<std::endl;
+                        x=Micola.Front();
+                        std::cout<<x<<std::endl;
+                        Micola.Dequeue();
                     }
-                    micola.Dequeue();
                     system("pause");
                     break;
 
             case 3: system("cls");
-                    if(micola.Vacia()){
-                        cout<<"La cola esta vacia."<<endl;
+                    if(Micola.vacia()){
+                        std::cout<<"La cola esta vacia."<<std::endl;
                     }else{
-                        cout<<"Ingresa los datos de la solicitud que deseas buscar: "<<endl;
-                        cin>>X;
-                            if(micola.Buscar(X)== 0){
-                                 cout<<"\n\tsolicitud encontrada! No hay constancias antes de ti."<<endl<<endl;
-                            }else if(micola.Buscar(X)>=0){
-                                cout<<"\n\tsolicitud encontrada!\nContancias restantes: "<<micola.Buscar(X)<<endl<<endl;
+                        std::cout<<"Ingresa los datos de la solicitud que deseas buscar: "<<std::endl;
+                        std::cin>>x;
+                            if(Micola.Buscar(x)==0){
+                                 std::cout<<"\n\tsolicitud encontrada! No hay constancias antes de ti."<<std::endl<<std::endl;
+                            }else if(Micola.Buscar(x)>=0){
+                                std::cout<<"\n\tsolicitud encontrada!\nContancias restantes: #"<<Micola.Buscar(x)<<std::endl<<std::endl;
                             }else{
-                                cout<<"\nConstancia no encontrada, favor de solicitar una."<<endl<<endl;
+                                std::cout<<"\nConstancia no encontrada, favor de solicitar una."<<std::endl<<std::endl;
                             }
                     }
                     system("pause");
